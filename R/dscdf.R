12771879354f0d2b090b830b167774711006603c
rgbeblavy <157547237+rgbeblavy@users.noreply.github.com>
2025-08-22 10:38:07 -0400

    Add files via upload

--- R/dscdf.R
+++ R/dscdf.R
@@ -1,8 +1,5 @@
-# library(MASS); library(parallel); library(foreach); library(doParallel); library(ggplot2)
+library(MASS); library(parallel); library(foreach); library(doParallel); library(ggplot2)
 
-### This is a copy of the functions from the CMMB R package.
-### These will be needed for running the functions in tslc.R, but are not new for this R package.
-### See the following link: https://github.com/mbsohn/cmmb
 get.init.lambda <- function(n.sample, n.feature, tol){
   f <- function(x, p) {(qnorm(1-x/p))^4 + 2*((qnorm(1-x/p))^2) - x;}
   k <- uniroot(f, lower=0, upper=n.feature-1, tol=tol, p=n.feature)$root
@@ -10,14 +7,14 @@ get.init.lambda <- function(n.sample, n.feature, tol){
   return(lambda_0)
 }
 
-comp_lasso_probit <- function(y, x, l_constraint, lam, mean_value, tol, max.iter){
+comp_lasso_probit <- function(y, x, l_constraint, lam, tol, max.iter, mean_value){
   n <- nrow(x); p <- ncol(x)
   q <- 2*y-1; bet0 <- rep(0, p)
   gramC <- crossprod(l_constraint); diagC <- diag(gramC); dediagC <- gramC - diag(diagC)
-  iter <- 0; ksi <- 0; ksi0 <- 1
+  mu <- 1; iter <- 0; ksi <- 0; ksi0 <- 1
   while (sum(abs(ksi-ksi0))>tol & iter<max.iter){
     ksi0 <- ksi; iter2 <- 0
-    mu <- 1; bet <- rep(1,p)/p
+    bet <- rep(1,p)/p
     while (sum(abs(bet-bet0))>tol & iter2<max.iter){
       bet0 <- bet
       xbeta <- x %*% bet0
@@ -62,7 +59,7 @@ comp_lasso_probit <- function(y, x, l_constraint, lam, mean_value, tol, max.iter
   return(list(beta=bet, u=u, LMBD=LMBD))
 }
 
-comp_slr_probit <- function(y, X, contr, lam0, flag_mv, tol, max.iter){
+comp_slr_probit <- function(y, X, contr, lam0, tol, max.iter, mean_value){
   n <- nrow(X); p <- ncol(X); contr2 <- t(as.matrix(contr))
   if(is.null(lam0)) lam0 <- get.init.lambda(n, p, tol)
   sigma <- 1; sigma_2 <- 1; sigma_s <- 0.5; iter <- 1
@@ -70,7 +67,7 @@ comp_slr_probit <- function(y, X, contr, lam0, flag_mv, tol, max.iter){
     iter <- iter + 1
     sigma <- (sigma_s + sigma_2)/2
     lam <- sigma*lam0
-    rslt <- comp_lasso_probit(y, X, contr2, lam, flag_mv, tol, max.iter)
+    rslt <- comp_lasso_probit(y, X, contr2, lam, tol, max.iter, mean_value)
     bet2 <- rslt$beta
     u <- rslt$u
     LMBD <- rslt$LMBD
@@ -84,7 +81,7 @@ comp_slr_probit <- function(y, X, contr, lam0, flag_mv, tol, max.iter){
   return(list(beta=bet, u=u, LMBD=LMBD, sigma=sigma, lambda0=lam0))
 }
 
-comp.debias.probit <- function(y, M, x, p.adj.method="BH", lam0=NULL, flag_mv=FALSE, tol=1e-3, max.iter=2000){
+comp.debias.probit <- function(y, M, x=NULL, p.adj.method="BH", lam0=NULL, tol=1e-3, max.iter=2000, mean_value=FALSE){
   if(!is.vector(y)) y <- as.vector(y)
   n <- length(y); k <- ncol(M)
   if(is.null(x)){
@@ -98,7 +95,7 @@ comp.debias.probit <- function(y, M, x, p.adj.method="BH", lam0=NULL, flag_mv=FA
   n.vrs <- 1 + k + n.x
   contr <- c(0, rep(1/sqrt(k), k), rep(0, n.x))
   Z.til <- Z %*% (diag(n.vrs)-tcrossprod(contr))
-  est.param <- comp_slr_probit(y, Z.til, contr, lam0, flag_mv, tol, max.iter)
+  est.param <- comp_slr_probit(y, Z.til, contr, lam0, tol, max.iter, mean_value)
   gam0 <- est.param$lambda0/3
   # zbeta <- Z.til %*% est.param$beta; q <- 2*y-1
   Sig <- crossprod(Z.til, est.param$LMBD%*%Z.til)/n
@@ -131,107 +128,98 @@ comp.debias.probit <- function(y, M, x, p.adj.method="BH", lam0=NULL, flag_mv=FA
   rslt.df <- data.frame(Est=debias.B, SD=sd.debias.B, pval=2*pnorm(abs(debias.B)/sd.debias.B, lower.tail=FALSE))
   rslt.df$adjp <- p.adjust(rslt.df$pval, method=p.adj.method)
   if(n.x > 0){
-    rownames(rslt.df) <- c("Intercept", paste0("M", 1:k), paste0("X", 1:n.x))
+    rownames(rslt.df) <- c("Intercept", paste0("F", 1:k), paste0("X", 1:n.x))
   } else{
-    rownames(rslt.df) <- c("Intercept", paste0("M", 1:k))
+    rownames(rslt.df) <- c("Intercept", paste0("F", 1:k))
   }
   return(rslt.df)
 }
 
-lasso_probit <- function(y, x, lam, mean_value, tol, max.iter){
-  n <- nrow(x); p <- ncol(x)
-  q <- 2*y-1; bet0 <- rep(0, p)
-  iter1 <- 0
-  mu <- 1; bet <- rep(1,p)/p
-  while (sum(abs(bet-bet0))>tol & iter1<max.iter){
-    bet0 <- bet
-    xbeta <- x %*% bet0
-    phi <- dnorm(q*xbeta); PHI <- pnorm(q*xbeta)
-    lmbd <- q*phi/PHI; LMBD <- diag(as.numeric(lmbd*(xbeta+lmbd)))
-    #u <- xbeta + solve(LMBD) %*% lmbd
-    u <- xbeta + ginv(LMBD) %*% lmbd
-    gramX <- crossprod(x, LMBD%*%x); diagX <- diag(gramX); dediagX <- gramX - diag(diagX)
-    covXY <- crossprod(x, LMBD%*%u)
-    term0 <- (covXY-dediagX%*%bet)/n
-    term2 <- diagX/n
-    for(j in 1:p){
-      term1 <- sign(term0[j])*max(0, abs(term0[j])-lam)
-      bet[j] <- term1/term2[j]
-      dif <- bet[j] - bet0[j]
-      term0 <- term0 - dediagX[,j]*dif/n
-    }
-    if(mean_value==TRUE){
-      stp <- bet-bet0; t <- 1
-      xbeta <- x %*% bet
-      phi <- dnorm(q*xbeta); PHI <- pnorm(q*xbeta)
-      lmbd <- q*phi/PHI
-      ll_cur <- sum(log(PHI))
-      eps <- crossprod(lmbd, x%*%stp); ll_new <- ll_cur + abs(eps)
-      iter2 <- 1
-      while ((ll_new+0.3*t*eps>ll_cur) & iter2<max.iter){
-        bet_new <- bet0 + t*stp
-        xbeta_new <- x %*% bet_new
-        PHI_new <- pnorm(q*xbeta_new)
-        ll_new <- sum(log(PHI_new))
-        t <- 0.9*t
-        iter2 <- iter2 + 1
+######################################
+comp_lasso <- function(y, x, l_constraint, lam, tol, max.iter){
+  n <- nrow(x); p <- ncol(x); k <- dim(l_constraint)[1]
+  gramC <- crossprod(l_constraint); gramX <- crossprod(x)
+  diagC <- diag(gramC); diagX <- diag(gramX)
+  dediagC <- gramC - diag(diagC); dediagX <- gramX - diag(diagX)
+  covXY <- crossprod(x, y)
+  mu <- 1; bet <- rep(1,p)/p; bet0 <- rep(0,p); iter <- 0
+  ksi <- rep(0,k); ksi0 <- rep(1,k)
+  term0 <- (covXY-dediagX%*%bet)/n - mu*(t(l_constraint)%*%ksi + dediagC%*%bet)
+  term2 <- diagX/n + mu*diagC
+  while (sum(abs(ksi-ksi0))>tol && iter<max.iter){
+    ksi0 <- ksi; iter2 <- 0; bet0 <- bet0 + 1
+    while (sum(abs(bet-bet0))>tol && iter2<1000){
+      bet0 <- bet
+      for(j in 1:p){
+        term1 <- sign(term0[j])*max(0, abs(term0[j])-lam)
+        bet[j] <- term1/term2[j]
+        dif <- bet[j] - bet0[j]
+        term0 <- term0 - dediagX[,j]*dif/n - dediagC[,j]*dif*mu
       }
-      bet <- bet_new
+      iter2 <- iter2 + 1
     }
-    iter1 <- iter1 + 1
+    dif2 <- l_constraint%*%bet
+    ksi <- ksi + dif2
+    term0 <- term0 - mu*t(l_constraint)%*%dif2
+    iter <- iter + 1
+    return(bet)
   }
-  return(list(beta=bet, u=u, LMBD=LMBD))
 }
 
-slr_probit <- function(y, X, lam0, flag_mv, tol, max.iter){
+### Scaled lasso for compositional data
+comp_slr <- function(y, X, contr, lam0, tol, max.iter){
   n <- nrow(X); p <- ncol(X)
+  mn.y <- mean(y); mn.X <- colMeans(X)
+  cent.X <- (X-tcrossprod(rep(1, n), mn.X))
+  contr2 <- t(as.matrix(contr))
+  cent.y <- y-mn.y
   if(is.null(lam0)) lam0 <- get.init.lambda(n, p, tol)
   sigma <- 1; sigma_2 <- 1; sigma_s <- 0.5; iter <- 1
   while (abs(sigma-sigma_s)>(tol*10) & iter<(max.iter/20)){
     iter <- iter + 1
     sigma <- (sigma_s + sigma_2)/2
     lam <- sigma*lam0
-    rslt <- lasso_probit(y, X, lam, flag_mv, tol, max.iter)
-    bet2 <- rslt$beta
-    u <- rslt$u
-    LMBD <- rslt$LMBD
-    s <- sum(abs(bet2)>tol); s <- min(s, n-1)
-    sigma_s <- sqrt(t(u-X%*%bet2) %*% LMBD %*% (u-X%*%bet2))/sqrt(n-s-1)
+    bet2 <- comp_lasso(cent.y, cent.X, contr2, lam, tol, max.iter)
+    s <- sum(abs(bet2)>0.001)
+    s <- min(s, n-1)
+    sigma_s <- base::norm(cent.y-cent.X%*%bet2, type="2")/sqrt(n-s-1)
     sigma_2 <- sigma
   }
-  if(iter==max.iter) print("Not converge!")
+  if(iter>max.iter) print("Not converge!")
   sigma <- sigma_s
   bet <- bet2
-  return(list(beta=bet, u=u, LMBD=LMBD, sigma=sigma, lambda0=lam0))
+  intercp <- mn.y - mn.X%*%bet
+  return(list(beta=bet, intercept=intercp, sigma=sigma, lambda0=lam0))
 }
 
-debias.probit <- function(y, M, x, p.adj.method="BH", lam0=NULL, flag_mv=FALSE, tol=1e-3, max.iter=2000){
+comp.debias <- function(y, M, x=NULL, p.adj.method="BH", lam0=NULL, tol=1e-3, max.iter=2000){
   if(!is.vector(y)) y <- as.vector(y)
   n <- length(y); k <- ncol(M)
   if(is.null(x)){
-    Z <- cbind(1, log(M))
+    Z <- log(M)
     n.x <- 0
   } else{
-    Z <- cbind(1, log(M), x)
+    Z <- cbind(log(M), x)
     if(!is.matrix(x)) x <- as.matrix(x)
     n.x <- ncol(x)
   }
-  n.vrs <- 1 + k + n.x
-  est.param <- slr_probit(y, Z, lam0, flag_mv, tol, max.iter)
-  gam0 <- est.param$lambda0
-  Sig <- crossprod(Z, est.param$LMBD%*%Z)/n
+  n.vrs <- k + n.x
+  contr <- c(rep(1/sqrt(k), k), rep(0, n.x))
+  Z.til <- Z %*% (diag(n.vrs)-tcrossprod(contr))
+  est.param <- comp_slr(y, Z.til, contr, lam0, tol, max.iter)
+  cent.y <- scale(y, scale=FALSE)
+  cent.Z.til <- scale(Z.til, scale=FALSE)
+  gam0 <- est.param$lambda0/3; Sig <- crossprod(cent.Z.til)/n
   Sig2 <- Sig - diag(diag(Sig))
-  Q <- diag(n.vrs)
-  M.hat <- matrix(0, n.vrs, n.vrs)
+  Q <- diag(n.vrs) - tcrossprod(contr)
+  M.til <- matrix(0, n.vrs, n.vrs)
   for(i in 1:n.vrs){
     gam <- gam0/2
-    stp.rho <- max(abs(Sig2))/diag(Sig)
-    stp.crit <- stp.rho/(1+stp.rho)
-    while(gam<stp.crit[i]){
+    while(gam<0.5){
       gam <- gam*2
       mi <- rep(1,n.vrs)
       mi0 <- rep(0,n.vrs)
-      iter <- 1
+      iter <- 1;
       while(sum(abs(mi-mi0))>tol & iter<max.iter){
         mi0 <- mi
         for(j in 1:n.vrs){
@@ -242,17 +230,25 @@ debias.probit <- function(y, M, x, p.adj.method="BH", lam0=NULL, flag_mv=FALSE, 
       }
       if(iter<max.iter) break
     }
-    M.hat[i,] <- mi
+    M.til[i,] <- mi
   }
-  debias.B <- est.param$beta + M.hat%*%t(Z)%*%(est.param$LMBD%*%(est.param$u-Z%*%est.param$beta))/n
-  cov.debias.B <- as.numeric(est.param$sigma^2)*M.hat%*%Sig%*%t(M.hat)/n
+  M.til <- Q%*%M.til
+  debias.B <- est.param$beta + M.til%*%t(cent.Z.til)%*%(cent.y-cent.Z.til%*%est.param$beta-rep(est.param$intercept,n))/n
+  cov.debias.B <- as.numeric(est.param$sigma^2)*M.til%*%Sig%*%t(M.til)/n
   sd.debias.B <- sqrt(diag(cov.debias.B))
   rslt.df <- data.frame(Est=debias.B, SD=sd.debias.B, pval=2*pnorm(abs(debias.B)/sd.debias.B, lower.tail=FALSE))
   rslt.df$adjp <- p.adjust(rslt.df$pval, method=p.adj.method)
   if(n.x > 0){
-    rownames(rslt.df) <- c("Intercept", paste0("M", 1:k), paste0("X", 1:n.x))
+    rownames(rslt.df) <- c(paste0("T", 1:k), paste0("X", 1:n.x))
   } else{
-    rownames(rslt.df) <- c("Intercept", paste0("M", 1:k))
+    rownames(rslt.df) <- c(paste0("T", 1:k))
   }
   return(rslt.df)
 }
+
+
+
+
+
+
+
--- R/tslc.R
+++ R/tslc.R
@@ -19,14 +19,13 @@ get.taxa <- function (func) {
   all_func_names <- func$X..Pathway
   taxa_names <- all_func_names[grepl("|", all_func_names, fixed = TRUE)]
 
-  ### Step 3:
+  ### Step 3: Construct data frame with species from each pathway name
   species_df <- data.frame("Species_Name" = 0,
                            "X..Pathway" = taxa_names)
 
   for (i in 1:length(taxa_names)) {
 
     if (grepl("s__", taxa_names[i], fixed = TRUE) == TRUE) {
-      #species_df[i, 1] <- strsplit(taxa_names[i], split = ".s__", fixed = TRUE)[[1]][2]
       species_df[i, 1] <- strsplit(taxa_names[i], split = "s__", fixed = TRUE)[[1]][2]
     }
 
@@ -61,59 +60,47 @@ get.taxa <- function (func) {
 #' Run The Two-Stagewise Method for Differential Abundance Analysis
 #'
 #' This is a function that takes a vector indicating the group membership, the functional profile,
-#' the taxonomic profile, a list containing all taxa associated with each function, and a matrix of covariates
-#' and runs the two-stagewise approach to finding differentially abundant taxa given differentially
-#' abundant functions.
+#' the taxonomic profile, a list containing all taxa associated with each function, and a matrix of covariates.
+#' It runs the two-stagewise approach to finding DA taxa given DA functions and returns a list of DA taxa
+#' assocaiated with each DA function.
 #'
 #' @param y A vector with elements from {0, 1} to indicate which population each sample belongs to. The order
 #' of the elements of y must match the order of the samples in the rows of the functional profile, taxonomic
 #' profile, and matrix of covariates.
-#' @param fun_prof A matrix with the samples as rows and pathways as columns. Note that the abundances of
-#' each sample must be converted to proportions in order for this to work. Also, the order of the rows
-#' must match the order of the samples in the response vector.
-#' @param tax_prof A matrix with the samples as rows and taxa as columns. Note that the abundances of each sample
-#' must be converted to proportions in order for this to work. The rownames of the taxonomic profile must
-#' match up to the rownames of the functional profile.
+#' @param fun_prof A matrix with the data for the functional profile. The rows must be samples with a column
+#' Sample_Name to indicate the sample IDs. The remaining columns must be for the pathways. Note that the
+#' abundances of each sample must be converted to proportions in order for this to work. Also, the order of
+#' the rows must match the order of the samples in the response vector.
+#' @param tax_prof A matrix with the data for the taxonomic profile. The rows must be samples with a column
+#' Sample_Name to indicate the sample IDs. The remaining columns must be for the pathways. Note that the
+#' abundances of each sample must be converted to proportions in order for this to work. Also, the order of
+#' the rows must match the order of the samples in the response vector.
 #' @param Fjs A list where the names of the list are the same as the column names of the functional profile,
 #' and each element of the list is a vector indicating which microbes in the taxonomic profile are associated
 #' with the corresponding function. The elements of the vector can be given as either column indices or names.
 #' @param x A matrix where the samples are rows and the columns are covariates to be adjusted for in the model.
 #' The rownames must match up to those of the functional and taxonomic profile, and any NA values must be
 #' removed from this matrix before running the method.
-#' @param cutoff A number to indicate which threshold the adjusted p-value should be cutoff at. All taxa with
-#' an adjusted p-value below this threshold will be detected as differentially abundant. To correct for
+#' @param pwy_cutoff A number to indicate which threshold the p-value for the pathways should be cutoff at. All
+#' pathways with a p-value below this threshold will be detected as DA.
+#' @param tax_cutoff A number to indicate which threshold the p-value for the taxa should be cutoff at. All
+#' taxa with a p-value below this threshold will be detected as DA. To correct for
 #' multiple comparisons, we use the Benjamini-Hochberg method.
 #'
-#' @return This function returns a list with the following elements.
+#' @return This function returns the following:
 #'
-#' da.taxa: A list where the names are each function identified as differentially abundant and the elements
+#' associated.da.taxa: A list where the names are each function identified as differentially abundant and the elements
 #' of the list are vectors containing all associated differentially abundant taxa.
 #'
-#' result: A data frame where the columns are each taxa name, their nominal p-value, and adjusted p-value.
-two.stage.da.analysis <- function (y, fun_prof, tax_prof, Fjs, x, cutoff = 0.20) {
-  num.fun <- ncol(fun_prof); num.taxa <- ncol(tax_prof)
-  pvals <- rep(1, num.taxa); adjp <- rep(1, num.taxa)
-  names(pvals) <- colnames(tax_prof); names(adjp) <- colnames(tax_prof)
-
-  if (!is.matrix(fun_prof)) {
-    return("Error: Functional Profile Must Be a Matrix.")
-  }
-
-  if (sum(rowSums(fun_prof)) != nrow(fun_prof)) {
-    return("Error: Each row of functional profile must be a composition.")
-  }
-
-  if (!is.matrix(tax_prof)) {
-    return("Error: Taxonomic Profile Must Be a Matrix.")
-  }
-
-  if (sum(rowSums(tax_prof)) != nrow(tax_prof)) {
-    return("Error: Each row of taxonomic profile must be a composition.")
-  }
+two.stage.da.analysis <- function (y, fun_prof, tax_prof, Fjs, x, pwy_cutoff = 0.05, tax_cutoff = 0.10) {
 
   if (!is.null(x)) {
     if (!is.matrix(x)) {
-      return("Error: Covariates must be given as a matrix.")
+      x <- as.matrix(x)
+    }
+
+    if (is.null(colnames(x))) {
+      colnames(x) <- paste0("X", 1:ncol(x))
     }
   }
 
@@ -133,52 +120,61 @@ two.stage.da.analysis <- function (y, fun_prof, tax_prof, Fjs, x, cutoff = 0.20)
     return("Error: Number of samples in functional profile do not match number of variables in taxonomic profile.")
   }
 
-  ### First Stage: Identify DA Functions
-  rslt.F <- comp.debias.probit(y = y, M = fun_prof, x = x)
-  da.fun.nums <- which(rslt.F[,"adjp"] < cutoff) - 1 # The -1 is because of the intercept
-  da.fun.nums <- da.fun.nums[da.fun.nums > 0]
-  da.fun.nums <- da.fun.nums[which(da.fun.nums <= num.fun)]
 
-  if (length(da.fun.nums) == 0) {
-    return("No taxa are identified as differentially abundant.")
+  ####################### First Stage: Identify DA Functions #######################
+  F.prof <- fun_prof %>%
+    dplyr::select(-Sample_Name) %>%
+    as.matrix()
+
+  if (sum(rowSums(F.prof)) != nrow(F.prof)) {
+    return("Error: Each row of functional profile must be a composition.")
   }
 
-  da.fun.names <- colnames(fun_prof)[da.fun.nums]
+  rslt <- comp.debias.probit(y, F.prof, x)
+  rownames(rslt) <- c("Intercept", colnames(F.prof), colnames(x))
 
-  ### Second Stage: Identify DA Taxa
-  ### For each function identified as DA, we perform DA analysis on the taxa associated with it
-  my.list <- vector(mode = "list", length = length(da.fun.names))
-  names(my.list) <- da.fun.names
+  ### Initialize a list to store DA species conditional on each DA pathway
+  da.pwy <- rslt[which(rslt$pval < pwy_cutoff),] %>% rownames()
 
-  for (fun.name in da.fun.names) {
-    taxa.associated <- Fjs[[fun.name]]
+  if (length(da.pwy) == 0) {
+    return("No functions are identified as differentially abundant.")
+  }
 
-    if (length(taxa.associated) < 2) {
+
+  ####################### Second Stage: DA Taxa Given DA Functions #######################
+  associated.da.species <- vector(mode = "list", length = length(da.pwy))
+  names(associated.da.species) <- da.pwy
+
+  for (pwy.name in da.pwy) {
+    ### Get species names associated with pathway
+    species.names <- Fjs[[pwy.name]]
+    species.names <- species.names[which(species.names %in% colnames(tax_prof))] # keep only those species that met filtering criterion
+
+    if (length(species.names) < 2) {
+      associated.da.species[[pwy.name]] <- 0
       next
     }
 
-    MM <- tax_prof[,taxa.associated]
-    MM <- sweep(MM, 1, rowSums(MM), "/")
+    ### Create a data frame with function abundance and the abundance of associated taxa
+    tmp2 <- tax_prof %>%
+      dplyr::select(Sample_Name, species.names) %>%
+      inner_join(fun_prof[,c("Sample_Name", pwy.name)], by = "Sample_Name")
+    #tmp2[,pwy.name] <- log(tmp2[,pwy.name]) # take log of function abundance
 
-    rslt.M <- comp.debias.probit(y = y, MM, x = NULL) # Perform second stage
+    ### Run the second stage
+    y.f <- tmp2[,pwy.name] %>% log()
+    M <- tmp2[,species.names] %>%
+      as.matrix() %>%
+      proportions(margin = 1)
 
-    rownames(rslt.M) <- c("Intercept", taxa.associated)
-    my.list[[fun.name]] <- taxa.associated[which(rslt.M[2:nrow(rslt.M),"adjp"] < cutoff)]
+    rslt2 <- comp.debias(y = y.f, M = M)
 
-    pvals[taxa.associated] <- rslt.M[1+(1:length(taxa.associated)),"pval"] # The 1+ is for the intercept
-    adjp[taxa.associated] <- rslt.M[1+(1:length(taxa.associated)),"adjp"] # The 1+ is for the intercept
+    ### Add species associated with pathway to the list
+    da.tax <- rslt2[which(rslt2$adjp < tax_cutoff),] %>% rownames()
+    associated.da.species[[pwy.name]] <- da.tax
+
   }
 
-  da.taxa.nums <- which(adjp < cutoff)
-  da.taxa.names <- colnames(tax_prof)[da.taxa.nums]
-
-  final.result.df <- data.frame("Taxon" = colnames(tax_prof),
-                                "P.value" = pvals,
-                                "Adj.p.value" = adjp)
-  final.result.df <- final.result.df[-which(final.result.df[,"Adj.p.value"] == 1),] # Remove taxa not associated with a DA function
-
-  return(list(da.taxa = my.list,
-              result = final.result.df))
+  return(associated.da.species)
 }
-
 
