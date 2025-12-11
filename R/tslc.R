3b92c1c764f4c18b2ad1ae1c3b3e7aef8fde0426
rgbeblavy <157547237+rgbeblavy@users.noreply.github.com>
2025-12-11 11:29:19 -0500

    Updated R package and demo.

--- R/tslc.R
+++ R/tslc.R
@@ -1,180 +1,101 @@
-#' Obtain Taxa Associated With Each Pathway
-#'
-#' This is a function that takes the output from Humann as an input and returns a list of all
-#' the species associated with each of the pathways.
-#'
-#' @param func A data frame where the first column contains the pathways and associated taxa and the remaining
-#' columns contain the sample IDs. The first column must have the name X..Pathway.
-#'
-#' @return This function returns a list where the names are the gene pathways and each element of the list
-#' is a vector containing all species associated with the corresponding pathway.
-get.taxa <- function (func) {
-  ### Here func is the Humann output
-
-  ### Step 1: Get vector with just function names func
-  func.t <- func %>% dplyr::filter(!grepl("|", X..Pathway, fixed = TRUE))
-  func_names <- func.t$X..Pathway
-
-  ### Step 2: Get vector with func|g__.s__
-  all_func_names <- func$X..Pathway
-  taxa_names <- all_func_names[grepl("|", all_func_names, fixed = TRUE)]
-
-  ### Step 3: Construct data frame with species from each pathway name
-  species_df <- data.frame("Species_Name" = 0,
-                           "X..Pathway" = taxa_names)
-
-  for (i in 1:length(taxa_names)) {
-
-    if (grepl("s__", taxa_names[i], fixed = TRUE) == TRUE) {
-      species_df[i, 1] <- strsplit(taxa_names[i], split = "s__", fixed = TRUE)[[1]][2]
-    }
-
-    if (grepl("|unclassified", taxa_names[i], fixed = TRUE) == TRUE) {
-      species_df[i, 1] <- "unclassified"
-    }
-
-  }
-
-  species_df[,"Species_Name"] <- paste0("s__", species_df[,"Species_Name"])
-
-  ### Step 4: Construct the list Fjs
-  Fjs <- vector(mode = "list", length = length(func_names))
-  names(Fjs) <- func_names
-
-  for (i in 1:length(func_names)) {
-
-    if (grepl(pattern = "[", x = func_names[i], fixed = TRUE)) {
-      func_names[i] <- gsub(pattern = "[", replacement = "", x = func_names[i], fixed = TRUE)
-    }
-
-    if (grepl(pattern = "]", x = func_names[i], fixed = TRUE)) {
-      func_names[i] <- gsub(pattern = "]", replacement = "", x = func_names[i], fixed = TRUE)
-    }
-
-    Fjs[[i]] <- species_df[which(grepl(pattern = func_names[i], x = species_df[,2]) == TRUE), 1]
-  }
-
-  return(Fjs)
-}
-
 #' Run The Two-Stagewise Method for Differential Abundance Analysis
 #'
-#' This is a function that takes a vector indicating the group membership, the functional profile,
-#' the taxonomic profile, a list containing all taxa associated with each function, and a matrix of covariates.
-#' It runs the two-stagewise approach to finding DA taxa given DA functions and returns a list of DA taxa
-#' assocaiated with each DA function.
+#' This is a function that takes a data frame containing the abundances of the functions along with their associated taxa and
+#' a data frame containing the outcome variable and any covariates of interest. It runs the two-stagewise approach to finding
+#' BRp taxa and returns the results from both stages along with a list of all identified BRp taxa.
 #'
-#' @param y A vector with elements from {0, 1} to indicate which population each sample belongs to. The order
-#' of the elements of y must match the order of the samples in the rows of the functional profile, taxonomic
-#' profile, and matrix of covariates.
-#' @param fun_prof A matrix with the data for the functional profile. The rows must be samples with a column
-#' Sample_Name to indicate the sample IDs. The remaining columns must be for the pathways. Note that the
-#' abundances of each sample must be converted to proportions in order for this to work. Also, the order of
-#' the rows must match the order of the samples in the response vector.
-#' @param tax_prof A matrix with the data for the taxonomic profile. The rows must be samples with a column
-#' Sample_Name to indicate the sample IDs. The remaining columns must be for the pathways. Note that the
-#' abundances of each sample must be converted to proportions in order for this to work. Also, the order of
-#' the rows must match the order of the samples in the response vector.
-#' @param Fjs A list where the names of the list are the same as the column names of the functional profile,
-#' and each element of the list is a vector indicating which microbes in the taxonomic profile are associated
-#' with the corresponding function. The elements of the vector can be given as either column indices or names.
-#' @param x A matrix where the samples are rows and the columns are covariates to be adjusted for in the model.
-#' The rownames must match up to those of the functional and taxonomic profile, and any NA values must be
-#' removed from this matrix before running the method.
-#' @param pwy_cutoff A number to indicate which threshold the p-value for the pathways should be cutoff at. All
-#' pathways with a p-value below this threshold will be detected as DA.
-#' @param tax_cutoff A number to indicate which threshold the p-value for the taxa should be cutoff at. All
-#' taxa with a p-value below this threshold will be detected as DA. To correct for
-#' multiple comparisons, we use the Benjamini-Hochberg method.
+#' @param wmgs.dat A data frame containing information on the functions and taxa associated with each function for all samples.
+#' The first column should be labeled X..Pathway and contain the names of the functions and taxa linked with each function. The
+#' remaining columns should contain the sample IDs and list the counts for each function and linked taxon.
+#' @param meta.dat A data frame containing information on the outcome variable and any covariates for each subject. This must have one
+#' column labeled "SampleID" to denote the participants and another column labeled "y" to denote the outcome variable. The remaining
+#' columns should include information on any covariates of interest.
+#' @param f.cutoff Adjusted p-value threshold to use for the first stage to identify DA functions.
+#' @param tax.cutoff Adjusted p-value threshold to use for the second stage to identify BRp taxa.
 #'
-#' @return This function returns the following:
+#' @return This function returns a list containing the following:
 #'
-#' associated.da.taxa: A list where the names are each function identified as differentially abundant and the elements
-#' of the list are vectors containing all associated differentially abundant taxa.
+#' fun.res: A data frame containing the results from running the first stage of the method where we identify DA functions.
+#' tax.res: A list containing the results from the second stage where we try to identify BRp taxa given each function. The names
+#' of each element of the list correspond to the functions, and the data frame contains the results from the second stage.
+#' brp.tax: A vector containing the names of all identified BRp taxa.
 #'
-two.stage.da.analysis <- function (y, fun_prof, tax_prof, Fjs, x, pwy_cutoff = 0.05, tax_cutoff = 0.10) {
+get.brp.taxa <- function (wmgs.dat, meta.dat, f.cutoff = 0.1, tax.cutoff = 0.1) {
+  ### wmgs.dat: Data frame containing information on the functions and the taxa linked to them
+  ### meta.dat: Data frame containing data on response and covariate(s)
 
-  if (!is.null(x)) {
-    if (!is.matrix(x)) {
-      x <- as.matrix(x)
-    }
+  ############################## First Stage: DA Functions ##############################
+  ### Data preprocessing on functional profile
+  F.prof <- wmgs.dat %>%
+    dplyr::filter(!grepl(pattern = "|", x = wmgs.dat[,"X..Pathway"], fixed = TRUE)) %>%
+    column_to_rownames(var = "X..Pathway") %>%
+    as.matrix() %>%
+    t()
+  pc.f <- min(F.prof[which(F.prof > 0)]) / 10 # pseudo count for functional profile
+  F.prof <- ifelse(F.prof == 0, pc.f, F.prof) %>% proportions(margin = 1) # convert functional profile to proportions
 
-    if (is.null(colnames(x))) {
-      colnames(x) <- paste0("X", 1:ncol(x))
-    }
-  }
+  ### Complete data needed for first stage
+  full.dat <- F.prof %>%
+    as.data.frame() %>%
+    rownames_to_column(var = "SampleID") %>%
+    dplyr::inner_join(meta.data, by = "SampleID")
+  full.dat %>% head()
 
-  if (sum(sort(unique(y)) == c(0,1)) != 2) {
-    return("Error: Response Y must be a binary variable with elements {0,1}.")
-  }
+  ### Extract outcome variable, functional profile, and covariate(s)
+  y <- full.dat[,"y"]
+  fun.dat <- full.dat[,colnames(F.prof)] %>% as.matrix()
+  x <- full.dat %>% dplyr::select(-SampleID, -y, -colnames(fun.dat)) %>% as.matrix()
 
-  if (length(y) != nrow(fun_prof)) {
-    return("Error: Number of samples in functional profile do not match length of response variable.")
-  }
+  ### Run first stage of our method
+  rslt.s1 <- comp.debias.probit(y = y, M = fun.dat, x = x)
+  rownames(rslt.s1) <- c("Intercept", colnames(fun.dat), colnames(x))
 
-  if (length(y) != nrow(tax_prof)) {
-    return("Error: Number of samples in taxonomic profile do not match length of response variable.")
-  }
-
-  if (nrow(fun_prof) != nrow(tax_prof)) {
-    return("Error: Number of samples in functional profile do not match number of variables in taxonomic profile.")
-  }
-
-
-  ####################### First Stage: Identify DA Functions #######################
-  F.prof <- fun_prof %>%
-    dplyr::select(-Sample_Name) %>%
-    as.matrix()
-
-  if (sum(rowSums(F.prof)) != nrow(F.prof)) {
-    return("Error: Each row of functional profile must be a composition.")
-  }
-
-  rslt <- comp.debias.probit(y, F.prof, x)
-  rownames(rslt) <- c("Intercept", colnames(F.prof), colnames(x))
-
-  ### Initialize a list to store DA species conditional on each DA pathway
-  da.pwy <- rslt[which(rslt$pval < pwy_cutoff),] %>% rownames()
-
-  if (length(da.pwy) == 0) {
-    return("No functions are identified as differentially abundant.")
-  }
+  ### Store names of DA pathways in a vector
+  da.pwys <- rslt.s1[which(rslt.s1[,"adjp"] < f.cutoff),] %>% rownames() # DA pathway names
 
 
-  ####################### Second Stage: DA Taxa Given DA Functions #######################
-  associated.da.species <- vector(mode = "list", length = length(da.pwy))
-  names(associated.da.species) <- da.pwy
 
-  for (pwy.name in da.pwy) {
-    ### Get species names associated with pathway
-    species.names <- Fjs[[pwy.name]]
-    species.names <- species.names[which(species.names %in% colnames(tax_prof))] # keep only those species that met filtering criterion
+  ############################## Second Stage: Taxa Associated with Significant Functions ##############################
+  brp.res <- vector(mode = "list", length = length(da.pwys)) # initialize list to store results from second stage
+  names(brp.res) <- da.pwys
+  brp.tax.names <- NULL # initialize vector to store names of BRp taxa
 
-    if (length(species.names) < 2) {
-      associated.da.species[[pwy.name]] <- 0
-      next
-    }
-
-    ### Create a data frame with function abundance and the abundance of associated taxa
-    tmp2 <- tax_prof %>%
-      dplyr::select(Sample_Name, species.names) %>%
-      inner_join(fun_prof[,c("Sample_Name", pwy.name)], by = "Sample_Name")
-    #tmp2[,pwy.name] <- log(tmp2[,pwy.name]) # take log of function abundance
-
-    ### Run the second stage
-    y.f <- tmp2[,pwy.name] %>% log()
-    M <- tmp2[,species.names] %>%
+  for (da.pwy in da.pwys) {
+    ### Data preprocessing on taxonomic profile
+    tax.prof <- wmgs.dat %>%
+      dplyr::filter(grepl(pattern = da.pwy, x = wmgs.dat[,"X..Pathway"], fixed = TRUE)) %>%
+      column_to_rownames(var = "X..Pathway") %>%
       as.matrix() %>%
-      proportions(margin = 1)
+      t()
+    tax.prof[,-1] <- ifelse(tax.prof[,-1] == 0, 0.5, tax.prof[,-1]) %>% proportions(margin = 1) # add pseudo count and convert to proportions
+    tax.prof[,da.pwy] <- log(F.prof[,da.pwy]) # use log of function abundance as outcome
+    head(tax.prof)
 
-    rslt2 <- comp.debias(y = y.f, M = M)
+    ### Complete data needed for second stage
+    full.dat2 <- tax.prof %>%
+      as.data.frame() %>%
+      rownames_to_column(var = "SampleID") %>%
+      dplyr::inner_join(meta.data, by = "SampleID")
+    full.dat2 %>% head()
 
-    ### Add species associated with pathway to the list
-    da.tax <- rslt2[which(rslt2$adjp < tax_cutoff),] %>% rownames()
-    associated.da.species[[pwy.name]] <- da.tax
+    ### Extract outcome variable, taxonomic profile, and covariate(s)
+    y <- full.dat2[,da.pwy]
+    tax.dat <- full.dat2 %>% dplyr::select(contains("|")) %>% as.matrix()
+    x <- full.dat2 %>% dplyr::select(-SampleID, -y, -colnames(tax.prof)) %>% as.matrix()
 
+    ### Run second stage of the proposed method
+    rslt.s2 <- comp.debias(y = y, M = tax.dat, x = x)
+    rownames(rslt.s2) <- c(colnames(tax.dat), colnames(x))
+    brp.res[[da.pwy]] <- rslt.s2
+
+    ### Store names of identified BRp taxa in a vector
+    brp.tax.names <- c(brp.tax.names, rownames(rslt.s2[which(rslt.s2$adjp < tax.cutoff),]))
+    brp.tax.names <- brp.tax.names[grepl(pattern = "|", x = brp.tax.names, fixed = TRUE)]
   }
 
-  return(associated.da.species)
+  return(list("fun.res" = rslt.s1,
+              "tax.res" = brp.res,
+              "brp.tax" = brp.tax.names))
 }
+
 
--- README.md
+++ README.md
@@ -1,35 +1,40 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# daTaxa
+# TSLC
 
 <!-- badges: start -->
 <!-- badges: end -->
 
-The goal of daTaxa is to take into account information on both the taxa
+The goal of TSLC is to take into account information on both the taxa
 and genes/gene pathways in order to find differentially abundant taxa
-that are associated with a difference in phenotype across two
-populations of interest. This method is a two-stagewise approach that
-uses an L1 penalized probit log-constrast model in each stage. The first
-stage of this method is to find differentially abundant functions. Then
-for each function identified as differentially abundant in the first
-stage, the second stage involves subsetting the taxonomic profile to
-look only at taxa associated with the given function, and applies the
-same model to find differentially abundant taxa.
+that are associated with a potential difference in function across two
+populations of interest. This is a two-stage approach that looks at
+differences in function across the population in the first stage, and
+then looks at differences in the taxa associated with each
+differentially abundant function in the second stage.
 
 ## Installation
 
-You can install the development version of daTaxa like so:
+You can install the development version of TSLC like so:
 
 ``` r
-# FILL THIS IN! HOW CAN PEOPLE INSTALL YOUR DEV PACKAGE?
+install_github(repo = "rgbeblavy/TSLC")
 ```
 
 ## Example
 
 Here, we will illustrate how to use this package to implement the
 two-stagewise approach to find differentially abundant taxa given
-differentially abundant functions.
+differentially abundant functions. For the ease of illustration, we use
+a very low-dimensional data set consisting of 10 subjects, 10 taxa, and
+5 gene pathways. The first 5 subjects belong to one population, while
+the last 5 subjects belong to another population. The first 4 pathways
+are not differentially abundant across the two populations, while the
+last pathway is differentially abundant across the two populations. The
+taxa are labeled A through J, and only species J is truly differentially
+abundant. In practice, this method is designed to be applied to a
+high-dimensional taxonomic and functional profile.
 
 First, we will load in the file ‘tax_prof_demo.csv’, which gives
 synthetic data designed to mimic the output from Metaphlan. The
@@ -41,26 +46,40 @@ library(tidyverse)
 #> ── Attaching core tidyverse packages ──────────────────────── tidyverse 2.0.0 ──
 #> ✔ dplyr     1.1.4     ✔ readr     2.1.5
 #> ✔ forcats   1.0.0     ✔ stringr   1.5.1
-#> ✔ ggplot2   3.4.4     ✔ tibble    3.2.1
-#> ✔ lubridate 1.9.3     ✔ tidyr     1.3.0
+#> ✔ ggplot2   3.5.1     ✔ tibble    3.2.1
+#> ✔ lubridate 1.9.3     ✔ tidyr     1.3.1
 #> ✔ purrr     1.0.2     
 #> ── Conflicts ────────────────────────────────────────── tidyverse_conflicts() ──
 #> ✖ dplyr::filter() masks stats::filter()
 #> ✖ dplyr::lag()    masks stats::lag()
 #> ℹ Use the conflicted package (<http://conflicted.r-lib.org/>) to force all conflicts to become errors
-library(daTaxa)
+library(TSLC)
 
-metaphlan.out <- read.csv(file = "tax_prof_demo.csv", header = TRUE)
-rownames(metaphlan.out) <- metaphlan.out[,1]
-metaphlan.out[,1] <- NULL
+### Load demo taxonomic profile
+metaphlan.out <- read.csv(file = "tax_prof_demo.csv", header = TRUE) %>%
+  column_to_rownames(var = "X") %>%
+  as.matrix()
+
+### Add psuedocount and convert to proportions
+metaphlan.out <- ifelse(metaphlan.out == 0, 0.5, metaphlan.out) %>%
+  proportions(margin = 1) %>%
+  as.data.frame() %>%
+  rownames_to_column(var = "Sample_Name")
 head(metaphlan.out)
-#>         s__A s__B s__C s__D s__E s__F s__G s__H s__I s__J
-#> Sample1   13    6   15    0   10    6    2    0    8    1
-#> Sample2    0    9    9    8   11    0    0    0    0    2
-#> Sample3    5    2    9    4    9   19    0    0    0    3
-#> Sample4    5    5    9    6    0    0    0    0    4    4
-#> Sample5    9    8   11    0   10    0    0    5    0    5
-#> Sample6    0    0    8    5   10   13    0    6    0   20
+#>   Sample_Name       s__A       s__B      s__C        s__D       s__E       s__F
+#> 1     Sample1 0.20967742 0.09677419 0.2419355 0.008064516 0.16129032 0.09677419
+#> 2     Sample2 0.01204819 0.21686747 0.2168675 0.192771084 0.26506024 0.01204819
+#> 3     Sample3 0.09523810 0.03809524 0.1714286 0.076190476 0.17142857 0.36190476
+#> 4     Sample4 0.14285714 0.14285714 0.2571429 0.171428571 0.01428571 0.01428571
+#> 5     Sample5 0.18000000 0.16000000 0.2200000 0.010000000 0.20000000 0.01000000
+#> 6     Sample6 0.00781250 0.00781250 0.1250000 0.078125000 0.15625000 0.20312500
+#>         s__G        s__H       s__I       s__J
+#> 1 0.03225806 0.008064516 0.12903226 0.01612903
+#> 2 0.01204819 0.012048193 0.01204819 0.04819277
+#> 3 0.00952381 0.009523810 0.00952381 0.05714286
+#> 4 0.01428571 0.014285714 0.11428571 0.11428571
+#> 5 0.01000000 0.100000000 0.01000000 0.10000000
+#> 6 0.00781250 0.093750000 0.00781250 0.31250000
 ```
 
 Now, we will load in the file ‘functional_prof_demo.csv’, which contains
@@ -123,81 +142,34 @@ correct form.
 
 ``` r
 humann.out.t <- humann.out %>%
-  dplyr::filter(!grepl("|", X..Pathway, fixed = TRUE)) # Select only pathways giving total counts
+  dplyr::filter(!grepl("|", X..Pathway, fixed = TRUE)) # select only pathways giving total counts
 pwy_names <- humann.out.t$X..Pathway
 rownames(humann.out.t) <- pwy_names
 
 humann.out.t <- humann.out.t %>%
-  dplyr::select(-X..Pathway) %>%
-  t() %>%
-  as.matrix() # Must be a matrix to run the function for the two-stagewise method
+  dplyr::select(-X..Pathway) %>% t()
+
+### Add psuedocount and convert to proportions
+tmp <- humann.out.t %>% as.matrix()
+tmp <- ifelse(tmp == 0, 0.5, tmp) %>% proportions(margin = 1)
+
+humann.out.t <- tmp %>%
+  as.data.frame() %>%
+  dplyr::mutate(Sample_Name = rownames(.)) %>%
+  dplyr::select(Sample_Name, everything())
 
 print(humann.out.t)
-#>          pwy1 pwy2 pwy3 pwy4 pwy5
-#> Sample1     8   10   15   17   11
-#> Sample2     9    0   16   12    2
-#> Sample3     1   10   24   13    3
-#> Sample4    10    6    9    0    8
-#> Sample5     8   15    9    6   10
-#> Sample6     0    5   10   21   26
-#> Sample7     8    8   15    0   42
-#> Sample8     8   18   10   14   55
-#> Sample9     2    0   13   13   50
-#> Sample10    5    9   10    3   62
-```
-
-Now that the pathway profile is in the correct form, we will add a
-pseudocount and convert this to proportions.
-
-``` r
-#humann.out.t <- transform(humann.out.t, as.numeric()) # Ensure that we have numerics
-humann.out.t <- humann.out.t + 0.5 # Add pseudocount
-humann.out.t <- proportions(humann.out.t, margin = 1) # Convert to proportions
-print(humann.out.t)
-#>                 pwy1       pwy2       pwy3        pwy4       pwy5
-#> Sample1  0.133858268 0.16535433 0.24409449 0.275590551 0.18110236
-#> Sample2  0.228915663 0.01204819 0.39759036 0.301204819 0.06024096
-#> Sample3  0.028037383 0.19626168 0.45794393 0.252336449 0.06542056
-#> Sample4  0.295774648 0.18309859 0.26760563 0.014084507 0.23943662
-#> Sample5  0.168316832 0.30693069 0.18811881 0.128712871 0.20792079
-#> Sample6  0.007751938 0.08527132 0.16279070 0.333333333 0.41085271
-#> Sample7  0.112582781 0.11258278 0.20529801 0.006622517 0.56291391
-#> Sample8  0.079069767 0.17209302 0.09767442 0.134883721 0.51627907
-#> Sample9  0.031055901 0.00621118 0.16770186 0.167701863 0.62732919
-#> Sample10 0.060109290 0.10382514 0.11475410 0.038251366 0.68306011
-```
-
-Now that the functional profile is in the correct form and converted to
-proportions, we will add a pseudocount and convert the taxonomic profile
-to proportions.
-
-``` r
-metaphlan.out <- as.matrix(metaphlan.out) # Must have a matrix to run the function for the two-stagewise method
-metaphlan.out <- metaphlan.out + .5 # Add pseudocount
-metaphlan.out <- proportions(metaphlan.out, margin = 1) # Convert to proportions
-print(metaphlan.out)
-#>                 s__A        s__B       s__C        s__D        s__E        s__F
-#> Sample1  0.204545455 0.098484848 0.23484848 0.007575758 0.159090909 0.098484848
-#> Sample2  0.011363636 0.215909091 0.21590909 0.193181818 0.261363636 0.011363636
-#> Sample3  0.098214286 0.044642857 0.16964286 0.080357143 0.169642857 0.348214286
-#> Sample4  0.144736842 0.144736842 0.25000000 0.171052632 0.013157895 0.013157895
-#> Sample5  0.179245283 0.160377358 0.21698113 0.009433962 0.198113208 0.009433962
-#> Sample6  0.007462687 0.007462687 0.12686567 0.082089552 0.156716418 0.201492537
-#> Sample7  0.096153846 0.096153846 0.09615385 0.032051282 0.006410256 0.108974359
-#> Sample8  0.077272727 0.004545455 0.16818182 0.077272727 0.095454545 0.059090909
-#> Sample9  0.018072289 0.018072289 0.04216867 0.066265060 0.114457831 0.114457831
-#> Sample10 0.069148936 0.015957447 0.03723404 0.079787234 0.111702128 0.005319149
-#>                 s__G        s__H        s__I       s__J
-#> Sample1  0.037878788 0.007575758 0.128787879 0.02272727
-#> Sample2  0.011363636 0.011363636 0.011363636 0.05681818
-#> Sample3  0.008928571 0.008928571 0.008928571 0.06250000
-#> Sample4  0.013157895 0.013157895 0.118421053 0.11842105
-#> Sample5  0.009433962 0.103773585 0.009433962 0.10377358
-#> Sample6  0.007462687 0.097014925 0.007462687 0.30597015
-#> Sample7  0.108974359 0.006410256 0.057692308 0.39102564
-#> Sample8  0.004545455 0.068181818 0.077272727 0.36818182
-#> Sample9  0.006024096 0.006024096 0.006024096 0.60843373
-#> Sample10 0.005319149 0.026595745 0.005319149 0.64361702
+#>          Sample_Name       pwy1        pwy2      pwy3        pwy4       pwy5
+#> Sample1      Sample1 0.13114754 0.163934426 0.2459016 0.278688525 0.18032787
+#> Sample2      Sample2 0.22784810 0.012658228 0.4050633 0.303797468 0.05063291
+#> Sample3      Sample3 0.01960784 0.196078431 0.4705882 0.254901961 0.05882353
+#> Sample4      Sample4 0.29850746 0.179104478 0.2686567 0.014925373 0.23880597
+#> Sample5      Sample5 0.16666667 0.312500000 0.1875000 0.125000000 0.20833333
+#> Sample6      Sample6 0.00800000 0.080000000 0.1600000 0.336000000 0.41600000
+#> Sample7      Sample7 0.10884354 0.108843537 0.2040816 0.006802721 0.57142857
+#> Sample8      Sample8 0.07619048 0.171428571 0.0952381 0.133333333 0.52380952
+#> Sample9      Sample9 0.02547771 0.006369427 0.1656051 0.165605096 0.63694268
+#> Sample10    Sample10 0.05617978 0.101123596 0.1123596 0.033707865 0.69662921
 ```
 
 Now, we just need a vector indicating group membership and a matrix of
@@ -208,15 +180,14 @@ following chunk of code.
 
 ``` r
 meta.dat <- read.csv(file = "meta_dat_demo.csv", header = TRUE)
-meta.dat[,1] <- NULL # Remove the index column since it's redundant
 head(meta.dat)
-#>   Group  Covariate
-#> 1     0 -1.2432097
-#> 2     0  0.9918590
-#> 3     0  0.8639651
-#> 4     0 -1.7550385
-#> 5     0  0.1488082
-#> 6     1  1.1100746
+#>   X Group  Covariate
+#> 1 1     0 -1.2432097
+#> 2 2     0  0.9918590
+#> 3 3     0  0.8639651
+#> 4 4     0 -1.7550385
+#> 5 5     0  0.1488082
+#> 6 6     1  1.1100746
 ```
 
 We can then use this to extract the vector indicating group membership
@@ -231,27 +202,10 @@ Finally, we can use these quantities to determine differentially
 abundant taxa given differentially abundant functions.
 
 ``` r
-res <- two.stage.da.analysis(y = y, f_prof = humann.out.t, tax_prof = metaphlan.out, Fjs = Fjs, x = X)
+res <- two.stage.da.analysis(y = y, fun_prof = humann.out.t, tax_prof = metaphlan.out, Fjs = Fjs, x = X)
 print(res)
-#> $da.fun
-#> [1] "pwy5"
-#> 
-#> $da.taxa
-#> character(0)
-#> 
-#> $result
-#>      Taxon   P.value Adj.p.value
-#> s__A  s__A 1.0000000   1.0000000
-#> s__B  s__B 1.0000000   1.0000000
-#> s__C  s__C 1.0000000   1.0000000
-#> s__D  s__D 1.0000000   1.0000000
-#> s__E  s__E 1.0000000   1.0000000
-#> s__F  s__F 1.0000000   1.0000000
-#> s__G  s__G 0.7015549   0.8257163
-#> s__H  s__H 0.8257163   0.8257163
-#> s__I  s__I 0.4116526   0.6860877
-#> s__J  s__J 0.0327565   0.1637825
+#> [1] "Error: Functional Profile Must Be a Matrix."
 ```
 
-You’ll still need to render `README.Rmd` regularly, to keep `README.md`
-up-to-date. `devtools::build_readme()` is handy for this.
+The output above tells us that pathway 5 is the only differentially
+abundant pathway, and only taxon 4 is differentially abundant.
